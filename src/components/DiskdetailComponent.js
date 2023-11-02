4b6ae38fa74430cbfdc057f5b506cdf032c61184
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-11-02 22:36:52 +0700

    Basically 20% done

---
+++ src/components/DiskdetailComponent.js
@@ -0,0 +1,85 @@
+import { Component } from "react";
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    CardImg,
+    CardImgOverlay,
+    CardText,
+    CardTitle,
+    Media,
+  } from "reactstrap";
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish: null,
+        }
+    }
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish });
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+          return (
+            <Card>
+                <CardHeader><h3 className="fw-bold">{dish.name}</h3></CardHeader>
+              <CardImg width={"25%"} top src={dish.image} alt={dish.name} />
+              <CardBody>
+                
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          );
+        else return <div></div>;
+      }
+
+      renderComment(dish) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardHeader><h3 className="fw-bold">Comments</h3></CardHeader>
+                    <CardBody>
+                        {dish.comments.map((comment) => {
+                            return (
+                                <div key={comment.id} className="list-unstyled">
+                                    <Media tag="li">
+                                        <Media body>
+                                            <p>{comment.comment}</p>
+                                            <p>
+                                                -- {comment.author} ,{" "}
+                                                {new Intl.DateTimeFormat("en-US", {
+                                                    year: "numeric",
+                                                    month: "short",
+                                                    day: "2-digit",
+                                                }).format(new Date(Date.parse(comment.date)))}
+                                            </p>
+                                        </Media>
+                                    </Media>
+                                </div>
+                            );
+                        })}
+                    </CardBody>
+                </Card>
+            )
+        }
+      }
+
+      render() {
+        return(
+            <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.props.selectedDish)}
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            {this.renderComment(this.props.selectedDish)}
+          </div>
+        </div>
+        )
+      }
+
+}
+
+export default DishDetail;
